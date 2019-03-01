50ae010f99c5797636feaad4ebf734be324df046
Marton Gunyho <andras.gunyho@aalto.fi>
2019-03-01 20:38:29 +0200

    Implement fading.

--- script.js
+++ script.js
@@ -1,21 +1,53 @@
+/*
+ * TODO: hide 'processing request' etc. in lower left corner
+ * instructions for firefox: https://support.mozilla.org/en-US/questions/1196927
+ */
 var index = 0;
-var urls = ["https://fyysikkokilta.fi", "https://www.example.com"];
-var iframes = document.getElementsByClassName("myIframe"); // TODO: rename class
-console.log(iframes);
-var iframeIdx = 0;
-var currentIframe = iframes[iframeIdx];
-var otherIframe = iframes[1 - iframeIdx];
+var urls = [
+    "https://fyysikkokilta.fi",
+    "https://www.example.com",
+    "naytto2.html", // local file
+    "http://hsl.trapeze.fi/traveller/web?command=fullscreen&id=FyyKiOK&title=lors&cols=2&extracolumn=stop",
+    "https://giphy.com/explore/random"
+];
+var topIframe = document.getElementById("topIframe");
+var botIframe = document.getElementById("botIframe");
+var currentIframeIsTop = false;
+var fadeTime = 500;
+var loadWaitTime = 500; // wait this long for pages to load in background
+
+//currentIframe.src = urls[]
 
 function newSite() {
-    currentIframe = otherIframe;
-    iframeIdx = 1 - iframeIdx;
-    otherIframe = iframes[iframeIdx];
+    //currentIframe = otherIframe;
+    //iframeIdx = 1 - iframeIdx;
+    //otherIframe = iframes[iframeIdx];
 
-    currentIframe.src = urls[index];
+    //otherIframe.src = urls[index];
     index = (index + 1) % urls.length;
+    var url = urls[index];
 
-    currentIframe.style["display"] = "block";
-    $(otherIframe).fadeOut(500)
+    //currentIframe.style["display"] = "block";
+    //$(otherIframe).fadeOut(500)
+    if(currentIframeIsTop) {
+        console.log("setting top url to " + url);
+        topIframe.src = url;
+    } else {
+        console.log("setting bottom url to " + url);
+        botIframe.src = url;
+    }
+
+    setTimeout(function() {
+        if(currentIframeIsTop) {
+            console.log("fade in");
+            $(topIframe).fadeIn(fadeTime);
+        } else {
+            console.log("fade out");
+            $(topIframe).fadeOut(fadeTime);
+        }
+        console.log("flipping variable");
+        currentIframeIsTop = !currentIframeIsTop;
+    }, loadWaitTime);
 }
 
 setInterval ( newSite, 5000);
