4066c3cf75b72027f9e127f5e5eb4474fb34297c
mgunyho <marci.gunyho@gmail.com>
2019-03-03 17:35:18 +0200

    Test custom transition.

--- script.js
+++ script.js
@@ -20,33 +20,53 @@ var urlManager = new URLManager();
 
 //TODO: lörssiä tähän, star-wars tyyliset star wipet, övereimmät powerpoint spinnaukset
 //TODO: consider moving to separate file
+
+$.effects.define( "kors", "toggle", function( options, done ) {
+  var show = options.mode === "show";
+ 
+  $( this )
+    .css( {
+        opacity: show ? 0 : 1,
+        //rotateX: 90, // TODO: can't be animated like this, see https://github.com/rstacruz/jquery.transit#readme, and section 'alternatives' therein
+    } )
+    .animate( {
+      opacity: show ? 1 : 0,
+      //rotateX: 0, //TODO
+    }, {
+      queue: false,
+      duration: options.duration,
+      easing: options.easing,
+      complete: done
+    } );
+} );
 var transitionEffectsWeighted = [
-    [{effect: "blind", duration: "slow"},                       1],
-    [{effect: "bounce", distance: 100, times: 5, duration: 1000},   1],
-    [{effect: "clip", duration: "slow"},                        1],
-    [{effect: "drop", direction: "left", duration: "slow"},     0.25],
-    [{effect: "drop", direction: "right", duration: "slow"},    0.25],
-    [{effect: "drop", direction: "up",   duration: "slow"},     0.25],
-    [{effect: "drop", direction: "down", duration: "slow"},     0.25],
-    //[{effect: "explode", duration: "slow"},   1], // doesn't work with 100% scale iframes, see https://stackoverflow.com/questions/13290086/jquery-explode-effect-not-working-with-percentage-width-height
-    [{effect: "fade", duration: "slow"},                        1],
-    [{effect: "fold", size: "10%", horizFirst: true, duration: "slow"},     0.5],
-    [{effect: "fold", size: "10%", horizFirst: false, duration: "slow"},    0.5],
-    //[{effect: "highlight", duration: "slow"}, 1], // doesn't seem to work really well
-    //[{effect: "puff", percent: 200, duration: "slow"},      1], // doesn't work with 100% widths..
-    [{effect: "pulsate", times: 10, duration: 1000},   0.1], // MAXIMUM EPILEPSY, good idea?
-    //[{effect: "shake", direction: "left", distance: 100, duration: "slow"},     1], // pretty dumb
-    //[{effect: "shake", direction: "up", distance: 100, duration: "slow"},     1], // pretty dumb
-    // effect: "size" is ~the same as effect: "scale"
-    [{effect: "size", scale: "box", origin: ["top", "left"], duration: "slow"},         0.2],
-    [{effect: "size", scale: "box", origin: ["top", "right"], duration: "slow"},        0.2],
-    [{effect: "size", scale: "box", origin: ["bottom", "right"], duration: "slow"},     0.2],
-    [{effect: "size", scale: "box", origin: ["bottom", "left"], duration: "slow"},      0.2],
-    [{effect: "size", scale: "box", origin: ["middle", "center"], duration: "slow"},    0.2],
-    [{effect: "slide", direction: "left", duration: "slow"},    0.25],
-    [{effect: "slide", direction: "right", duration: "slow"},   0.25],
-    [{effect: "slide", direction: "up",   duration: "slow"},    0.25],
-    [{effect: "slide", direction: "down", duration: "slow"},    0.25],
+    //[{effect: "blind", duration: "slow"},                       1],
+    //[{effect: "bounce", distance: 100, times: 5, duration: 1000},   1],
+    //[{effect: "clip", duration: "slow"},                        1],
+    //[{effect: "drop", direction: "left", duration: "slow"},     0.25],
+    //[{effect: "drop", direction: "right", duration: "slow"},    0.25],
+    //[{effect: "drop", direction: "up",   duration: "slow"},     0.25],
+    //[{effect: "drop", direction: "down", duration: "slow"},     0.25],
+    ////[{effect: "explode", duration: "slow"},   1], // doesn't work with 100% scale iframes, see https://stackoverflow.com/questions/13290086/jquery-explode-effect-not-working-with-percentage-width-height
+    //[{effect: "fade", duration: "slow"},                        1],
+    //[{effect: "fold", size: "10%", horizFirst: true, duration: "slow"},     0.5],
+    //[{effect: "fold", size: "10%", horizFirst: false, duration: "slow"},    0.5],
+    ////[{effect: "highlight", duration: "slow"}, 1], // doesn't seem to work really well
+    ////[{effect: "puff", percent: 200, duration: "slow"},      1], // doesn't work with 100% widths..
+    //[{effect: "pulsate", times: 10, duration: 1000},   0.1], // MAXIMUM EPILEPSY, good idea?
+    ////[{effect: "shake", direction: "left", distance: 100, duration: "slow"},     1], // pretty dumb
+    ////[{effect: "shake", direction: "up", distance: 100, duration: "slow"},     1], // pretty dumb
+    //// effect: "size" is ~the same as effect: "scale"
+    //[{effect: "size", scale: "box", origin: ["top", "left"], duration: "slow"},         0.2],
+    //[{effect: "size", scale: "box", origin: ["top", "right"], duration: "slow"},        0.2],
+    //[{effect: "size", scale: "box", origin: ["bottom", "right"], duration: "slow"},     0.2],
+    //[{effect: "size", scale: "box", origin: ["bottom", "left"], duration: "slow"},      0.2],
+    //[{effect: "size", scale: "box", origin: ["middle", "center"], duration: "slow"},    0.2],
+    //[{effect: "slide", direction: "left", duration: "slow"},    0.25],
+    //[{effect: "slide", direction: "right", duration: "slow"},   0.25],
+    //[{effect: "slide", direction: "up",   duration: "slow"},    0.25],
+    //[{effect: "slide", direction: "down", duration: "slow"},    0.25],
+    [{effect: "kors", direction: "down", duration: "slow"},    0.25],
 ];
 
 function newSite() {
