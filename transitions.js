2b9c44153b424d3f04fdcf4f48978126220670fa
Marton Gunyho <andras.gunyho@aalto.fi>
2019-03-15 18:53:14 +0200

    Re-enable all transitions.

--- transitions.js
+++ transitions.js
@@ -120,34 +120,34 @@ $.effects.define( "starwipe", "toggle", function( options, done ) {
 
 // list of all possible transitions
 var transitionEffectsWeighted = [
-    //[{effect: "blind", duration: "slow"},                       1],
-    //[{effect: "bounce", distance: 100, times: 5, duration: 1000},   1],
-    //[{effect: "clip", duration: "slow"},                        1],
-    //[{effect: "drop", direction: "left", duration: "slow"},     0.25],
-    //[{effect: "drop", direction: "right", duration: "slow"},    0.25],
-    //[{effect: "drop", direction: "up",   duration: "slow"},     0.25],
-    //[{effect: "drop", direction: "down", duration: "slow"},     0.25],
-    ////[{effect: "explode", duration: "slow"},   1], // doesn't work with 100% scale iframes, see https://stackoverflow.com/questions/13290086/jquery-explode-effect-not-working-with-percentage-width-height
-    //[{effect: "fade", duration: "slow"},                        1],
-    //[{effect: "fold", size: "10%", horizFirst: true, duration: "slow"},     0.5],
-    //[{effect: "fold", size: "10%", horizFirst: false, duration: "slow"},    0.5],
-    ////[{effect: "highlight", duration: "slow"}, 1], // doesn't seem to work really well
-    ////[{effect: "puff", percent: 200, duration: "slow"},      1], // doesn't work with 100% widths..
-    //[{effect: "pulsate", times: 10, duration: 1000},   0.1], // MAXIMUM EPILEPSY, good idea?
-    ////[{effect: "shake", direction: "left", distance: 100, duration: "slow"},     1], // pretty dumb
-    ////[{effect: "shake", direction: "up", distance: 100, duration: "slow"},     1], // pretty dumb
-    //// effect: "size" is ~the same as effect: "scale"
-    //[{effect: "size", scale: "box", origin: ["top", "left"], duration: "slow"},         0.2],
-    //[{effect: "size", scale: "box", origin: ["top", "right"], duration: "slow"},        0.2],
-    //[{effect: "size", scale: "box", origin: ["bottom", "right"], duration: "slow"},     0.2],
-    //[{effect: "size", scale: "box", origin: ["bottom", "left"], duration: "slow"},      0.2],
-    //[{effect: "size", scale: "box", origin: ["middle", "center"], duration: "slow"},    0.2],
-    //[{effect: "slide", direction: "left", duration: "slow"},    0.25],
-    //[{effect: "slide", direction: "right", duration: "slow"},   0.25],
-    //[{effect: "slide", direction: "up",   duration: "slow"},    0.25],
-    //[{effect: "slide", direction: "down", duration: "slow"},    0.25],
+    [{effect: "blind", duration: "slow"},                       1],
+    [{effect: "bounce", distance: 100, times: 5, duration: 1000},   1],
+    [{effect: "clip", duration: "slow"},                        1],
+    [{effect: "drop", direction: "left", duration: "slow"},     0.25],
+    [{effect: "drop", direction: "right", duration: "slow"},    0.25],
+    [{effect: "drop", direction: "up",   duration: "slow"},     0.25],
+    [{effect: "drop", direction: "down", duration: "slow"},     0.25],
+    //[{effect: "explode", duration: "slow"},   1], // doesn't work with 100% scale iframes, see https://stackoverflow.com/questions/13290086/jquery-explode-effect-not-working-with-percentage-width-height
+    [{effect: "fade", duration: "slow"},                        1],
+    [{effect: "fold", size: "10%", horizFirst: true, duration: "slow"},     0.5],
+    [{effect: "fold", size: "10%", horizFirst: false, duration: "slow"},    0.5],
+    //[{effect: "highlight", duration: "slow"}, 1], // doesn't seem to work really well
+    //[{effect: "puff", percent: 200, duration: "slow"},      1], // doesn't work with 100% widths..
+    [{effect: "pulsate", times: 10, duration: 1000},   0.1], // MAXIMUM EPILEPSY, good idea?
+    //[{effect: "shake", direction: "left", distance: 100, duration: "slow"},     1], // pretty dumb
+    //[{effect: "shake", direction: "up", distance: 100, duration: "slow"},     1], // pretty dumb
+    // effect: "size" is ~the same as effect: "scale"
+    [{effect: "size", scale: "box", origin: ["top", "left"], duration: "slow"},         0.2],
+    [{effect: "size", scale: "box", origin: ["top", "right"], duration: "slow"},        0.2],
+    [{effect: "size", scale: "box", origin: ["bottom", "right"], duration: "slow"},     0.2],
+    [{effect: "size", scale: "box", origin: ["bottom", "left"], duration: "slow"},      0.2],
+    [{effect: "size", scale: "box", origin: ["middle", "center"], duration: "slow"},    0.2],
+    [{effect: "slide", direction: "left", duration: "slow"},    0.25],
+    [{effect: "slide", direction: "right", duration: "slow"},   0.25],
+    [{effect: "slide", direction: "up",   duration: "slow"},    0.25],
+    [{effect: "slide", direction: "down", duration: "slow"},    0.25],
 
-    //[{effect: "spin", angle:  360, scale: 0.1, duration: "slow"},    0.5],
-    //[{effect: "spin", angle: -360, scale: 0.1, duration: "slow"},    0.5],
+    [{effect: "spin", angle:  360, scale: 0.1, duration: "slow"},    0.5],
+    [{effect: "spin", angle: -360, scale: 0.1, duration: "slow"},    0.5],
     [{effect: "starwipe", duration: 1000},    1],
 ];
