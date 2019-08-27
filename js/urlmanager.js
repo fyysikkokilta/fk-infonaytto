09828f33b9018a585478f848fcd6bfdf2ab799ee
Infonaytto-pc <viestintavastaava@fyysikkokilta.fi>
2019-08-27 23:20:11 +0300

    Update URL weights.

--- js/urlmanager.js
+++ js/urlmanager.js
@@ -12,11 +12,11 @@ var urlsWeighted = [
     [WappuURLGenerator, 1.],
     [PerjantaiURLGenerator, 0.2],
     //["html/kanttiinit.html", 2], //TODO
-    [TelegramURLGenerator, 9990.5],
+    [TelegramURLGenerator, 0.5],
     ["html/calendar/calendar.html", 1],
     //["https://kanttiinit.fi", 2], //TODO: see https://kitchen.kanttiinit.fi/menus?lang=fi&restaurants=52,12&days=2019-04-05
-    ["html/ruokalistat/kanttiinit.html?page=1", 1.5],
-    ["html/ruokalistat/kanttiinit.html?page=2", 1.5],
+    ["html/ruokalistat/kanttiinit.html?page=1", 2.5], //TODO: add parameter for duration, show kanttiinit for longer
+    ["html/ruokalistat/kanttiinit.html?page=2", 2.5],
     //["https://www.inkubio.fi/kiltiscam/", 0.5],
     //["https://www.inkubio.fi/kiltiscam/kiltahuone.jpg", 0.5], //TODO: own html file for this to fit image
 ];
