f95427ae6e62c5003f641ee106bc690d5dcc9856
Infonaytto-pc <viestintavastaava@fyysikkokilta.fi>
2019-08-27 23:19:31 +0300

    Change interval to 60 seconds.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // some options
 
-const TRANSITION_INTERVAL = 10; // wait this many seconds between pages
+const TRANSITION_INTERVAL = 60; // wait this many seconds between pages
 const LOAD_WAIT_TIME = 500; // wait this long for pages to load in background
 
 // this many of the last displayed urls are discarded when selecting the next one.
